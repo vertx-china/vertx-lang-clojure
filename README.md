15cfea9995d632eb26d16a79ee901e650769ca9e
chengenzhao001 <chengen.zhao@mail.mcgill.ca>
2018-01-10 16:58:18 +0800

    add doc generator we still need a translator for clojure language for now we use kotlin lang translator instead will change later

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Vert.x Clojure support
 TODO
 
 - [x] Auto-generate thin wrap APIs by using Codegen
-- [x] VerticleWrapper of generated APIs(ClojureVerticle for .clj suffix namespaces
+- [x] VerticleWrapper of generated APIs(ClojureVerticle for .clj suffix namespaces)
 - [x] ClojureVerticleFactory service
 - [x] Tests
 - [ ] Auto-generate docs by using Docgen
---
+++ vertx-lang-clojure/src/main/java/io/vertx/lang/clojure/ClojureDocGenerator.java
@@ -0,0 +1,66 @@
+package io.vertx.lang.clojure;
+
+import io.vertx.codetrans.CodeTranslator;
+import io.vertx.codetrans.lang.kotlin.KotlinLang;
+import io.vertx.docgen.Coordinate;
+import io.vertx.docgen.DocGenerator;
+import io.vertx.docgen.JavaDocGenerator;
+
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.VariableElement;
+
+public class ClojureDocGenerator implements DocGenerator {
+
+  private JavaDocGenerator javaGen = new JavaDocGenerator();
+  private CodeTranslator translator;
+
+  @Override
+  public void init(ProcessingEnvironment processingEnv) {
+    translator = new CodeTranslator(processingEnv);
+    javaGen.init(processingEnv);
+  }
+
+  @Override
+  public String getName() {
+    return "clojure";
+  }
+
+  @Override
+  public String renderSource(ExecutableElement elt, String source) {
+    KotlinLang lang = new KotlinLang();
+    try {
+      return translator.translate(elt, lang);
+    } catch (Exception e) {
+      System.out.println("Cannot generate " + elt.getEnclosingElement().getSimpleName() + "#" + elt.getSimpleName() + " : " + e.getMessage());
+      return "Code not translatable";
+    }
+  }
+
+  @Override
+  public String resolveTypeLink(TypeElement elt, Coordinate coordinate) {
+    return javaGen.resolveTypeLink(elt, coordinate);
+  }
+
+  @Override
+  public String resolveConstructorLink(ExecutableElement elt, Coordinate coordinate) {
+    return javaGen.resolveConstructorLink(elt, coordinate);
+  }
+
+  @Override
+  public String resolveMethodLink(ExecutableElement elt, Coordinate coordinate) {
+    return javaGen.resolveMethodLink(elt, coordinate);
+  }
+
+  @Override
+  public String resolveLabel(Element elt, String defaultLabel) {
+    return javaGen.resolveLabel(elt, defaultLabel);
+  }
+
+  @Override
+  public String resolveFieldLink(VariableElement elt, Coordinate coordinate) {
+    return javaGen.resolveFieldLink(elt, coordinate);
+  }
+}
