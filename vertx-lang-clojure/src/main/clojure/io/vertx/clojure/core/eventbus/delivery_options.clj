36ee122ef436cdffbfbe832b11f535886824a9e8
chengenzhao001 <chengen.zhao@mail.mcgill.ca>
2018-02-28 20:23:37 +0800

    fix adder method with kind.name == "MAP" error

--- vertx-lang-clojure/src/main/clojure/io/vertx/clojure/core/eventbus/delivery_options.clj
+++ vertx-lang-clojure/src/main/clojure/io/vertx/clojure/core/eventbus/delivery_options.clj
@@ -9,7 +9,7 @@
 
 (defn set-codec-name [^DeliveryOptions delivery-options codec-name] (.setCodecName delivery-options codec-name))
 (defn get-codec-name [^DeliveryOptions delivery-options] (.getCodecName delivery-options))
-(defn add-header [^DeliveryOptions delivery-options headers] (.addHeader delivery-options headers))
+(defn add-header [^DeliveryOptions delivery-options key headers] (.addHeader delivery-options key headers))
 (defn set-send-timeout [^DeliveryOptions delivery-options send-timeout] (.setSendTimeout delivery-options send-timeout))
 (defn get-send-timeout [^DeliveryOptions delivery-options] (.getSendTimeout delivery-options))
 (defn to-json [^DeliveryOptions delivery-options] (.toJson delivery-options))
