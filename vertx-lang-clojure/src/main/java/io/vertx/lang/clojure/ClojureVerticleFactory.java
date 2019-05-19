db596f74a8f8facbdcdc13cdd0566d8c1ffe3ef4
Sébastien Le Callonnec <sebastien@weblogism.com>
2019-05-19 19:32:03 +0100

    Another minor cleanup.

--- vertx-lang-clojure/src/main/java/io/vertx/lang/clojure/ClojureVerticleFactory.java
+++ vertx-lang-clojure/src/main/java/io/vertx/lang/clojure/ClojureVerticleFactory.java
@@ -36,22 +36,20 @@ public class ClojureVerticleFactory implements VerticleFactory {
   public Verticle createVerticle(String verticleName, ClassLoader classLoader) throws Exception {
 
     String ns = VerticleFactory.removePrefix(verticleName);
-
     boolean requireCompiling = false;
 
-    if(ns.endsWith("."+prefix())){
-
-      ns = ns.substring(0,ns.indexOf("."+prefix()));
+    if (ns.endsWith("." + prefix())) {
+      ns = ns.substring(0, ns.indexOf("." + prefix()));
       //check .clj source file exists, if file exists, Clojure will try to compile it
       //and concurrently compile clojure files may cause exception, it has to be serial
-      String filePath = ns.replace(".",File.separator) + "." + prefix();
-      if(classLoader.getResource(filePath)!=null){
+      String filePath = ns.replace(".", File.separator) + "." + prefix();
+      if (classLoader.getResource(filePath) != null) {
         requireCompiling = true;
       }
     }
 
     //change SNAKE_CASE to KEBAB_CASE since in the namespace, clojure uses Kebab case, while Snake case in file name.
-    ns = ns.replace("_","-");
+    ns = ns.replace("_", "-");
 
     return new ClojureVerticle(ns, requireCompiling);
   }
