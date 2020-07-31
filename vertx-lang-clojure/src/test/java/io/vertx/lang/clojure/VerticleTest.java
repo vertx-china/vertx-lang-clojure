5f3161b743f366ab3203e55d1de0d8c9e8df9143
Sébastien Le Callonnec <sebastien@weblogism.com>
2020-07-31 13:46:36 +0100

    Restore test.

--- vertx-lang-clojure/src/test/java/io/vertx/lang/clojure/VerticleTest.java
+++ vertx-lang-clojure/src/test/java/io/vertx/lang/clojure/VerticleTest.java
@@ -1,24 +1,25 @@
 package io.vertx.lang.clojure;
 
+import io.vertx.core.Future;
 import io.vertx.core.http.*;
 import io.vertx.test.core.VertxTestBase;
 import org.junit.Test;
 
 public class VerticleTest extends VertxTestBase {
 
-  @Test
-  public void testHttpServer() {
-//    vertx.deployVerticle("examples.simple_http_server.clj", ar -> {
-//      assertTrue(ar.succeeded());
-//      HttpClient client = vertx.createHttpClient(new HttpClientOptions());
-//      HttpClientRequest req = client.request(HttpMethod.GET, 8080, "localhost", "/");
-//      req.exceptionHandler(err -> fail());
-//      req.onSuccess(resp -> {
-//          assertEquals(200, resp.statusCode());
-//          testComplete();
-//      });
-//      req.end();
-//    });
-//    await();
-  }
+    @Test
+    public void testHttpServer() {
+        vertx.deployVerticle("examples.simple_http_server.clj", ar -> {
+            assertTrue(ar.succeeded());
+            System.out.println("**** DEployed ! ***");
+            HttpClient client = vertx.createHttpClient(new HttpClientOptions().setLogActivity(true));
+            Future<HttpClientResponse> res = client.get(8080, "localhost", "/");
+
+            res.onSuccess(resp -> {
+                assertEquals(200, resp.statusCode());
+                testComplete();
+            });
+        });
+        await();
+    }
 }
